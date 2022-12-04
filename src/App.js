579e6a09ff880f1c1821ec67ac7326f872708ccc
Amirhossein Mirsalari <amir.tk7@gmail.com>
2022-12-04 14:19:35 +0330

    exercise redux toolkit with json server to build TodoApp

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import CounterComp from "./components/CounterComp";
+import AddTodoForm from "./components/Todos/AddTodoForm";
+import TodoList from "./components/Todos/TodoList";
+import TotalCompleteItems from "./components/Todos/TotalCompleteTodo";
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <div className="App container">
+      {/* <h1>hi saheb !</h1> */}
+      {/* <CounterComp /> */}
+      <AddTodoForm />
+      <TodoList />
+      {/* <TotalCompleteItems /> */}
+    </div>
+  );
+}
+
+export default App;
